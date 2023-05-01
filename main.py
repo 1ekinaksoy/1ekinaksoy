320fd24b5921784512eccd96dfc962bb04741c07
1ekinaksoy <132273800+1ekinaksoy@users.noreply.github.com>
2023-05-01 16:33:25 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+gun_sayisi: int = int(input("Lütfen öğrenmek istediğiniz günün üzerine girin: "))
+
+if gun_sayisi > 7 or gun_sayisi < 1:
+      print("Hatali gun sayisi")
+elif gun_sayisi == 1:
+      print("Yevmu'l-Eḥad")
+elif gun_sayisi == 2:
+      print("Yevmu'l-İs̠neyn")
+elif gun_sayisi == 3:
+      print("Yevmu's̠-S̠ülās̠ā'")
+elif gun_sayisi == 4:
+      print("Yevmu'l-Erbi'ā'")
+elif gun_sayisi == 5:
+      print("Yevmu'l-Hamise")
+elif gun_sayisi == 6:
+      print("Yevmu'l-Cumu'a")
+elif gun_sayisi == 7:
+      print("Yevmu's-Sebt")
+
+
+
+
