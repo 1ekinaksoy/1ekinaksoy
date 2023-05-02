ecd5a8de8c6974e653b6cb6d7c279667914a624e
1ekinaksoy <132273800+1ekinaksoy@users.noreply.github.com>
2023-05-02 15:53:38 +0300

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,28 @@
+ay_sayisi: int = int(input("Lütfen öğrenmek istediğiniz ayın numarasını girin: "))
+
+if ay_sayisi == 1:
+    print("Muharrem")
+elif ay_sayisi == 2:
+    print("Safer")
+elif ay_sayisi == 3:
+    print("Rebiülevvel")
+elif ay_sayisi == 4:
+    print("Rebiülahir")
+elif ay_sayisi == 5:
+    print("Cemaziyelevvel")
+elif ay_sayisi == 6:
+    print("Cemaziyelahir")
+elif ay_sayisi == 7:
+    print("Recep")
+elif ay_sayisi == 8:
+    print("Şaban")
+elif ay_sayisi == 9:
+    print("Ramazan")
+elif ay_sayisi == 10:
+    print("Şevval")
+elif ay_sayisi == 11:
+    print("Zilkade")
+elif ay_sayisi == 12:
+    print("Zilhicce")
+else:
+    print("Yanlış bir değer girdiniz")
